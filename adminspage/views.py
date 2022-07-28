eff7768a98c41825fb175539f821d98e7543d2ff
Birendra-Shrestha <Shresthabirendra87@gmail.com>
2022-07-28 07:51:40 +0545

    Initial commit

---
+++ adminspage/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from users.auth import admin_only
+
+# Create your views here.
+@login_required
+@admin_only
+def admin_home(request):
+    return render(request,'admins/adminhome.html')
